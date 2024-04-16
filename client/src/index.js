01fcf1f6c0aa0a7526b2af0fd84c576a63abc278
satwik398 <73548217+satwik398@users.noreply.github.com>
2024-04-16 14:57:29 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import {BrowserRouter} from "react-router-dom";
 import {createRoot} from "react-dom/client";
-import App from "./App";
+import App from "client/src/App";
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
